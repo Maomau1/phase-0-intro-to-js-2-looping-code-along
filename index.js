bb92860af7759a72e963483343750d4697b80daf
maurice diong <mdiong00@gmail.com>
2022-10-04 22:07:55 -0400

    commit tree ce3feaecbce7bb1eefa19964f1d545f8ec9b18ef

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+  }
+
+  const countDown = function (x) {
+    for ( let i=x; i>=0; i--) {
+        console.log(i);
+    }
+  }
+
+  const writeCards = function (array,event) {
+    const newArray = [];
+    for (let i=0; i<array.length; i++) {
+        newArray[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!` ;
+
+    }
+    return newArray;
+  }
